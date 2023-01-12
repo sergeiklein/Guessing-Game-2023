2cf2eb9204b1373a8fa0ea9ce9a13811291a65a0
sergeiklein <sjklein088@gmail.com>
2023-01-12 07:26:59 -0800

    Game Completed

--- guessing game 2023/guessing game 2023/Program.cs
+++ guessing game 2023/guessing game 2023/Program.cs
@@ -6,17 +6,24 @@ Console.WriteLine("Welcome to Sok and Sergei's Guessing Game!");
 
 Console.WriteLine("Can you guess the correct number 1-10? ");
 
-int guessNumber = Convert.ToInt32(Console.ReadLine());
+int attempt = 1;
+while (attempt < 4)
+{
+    int guessNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Attempt #" + attempt);
+    attempt++;
 
-if (guessNumber.Equals("5"))
-{
-    Console.WriteLine("Thats Correct!!");
+    if (guessNumber.Equals(5))
+    {
+        Console.WriteLine("Thats Correct!!");
+    }
+    else if (guessNumber >= 10)
+    {
+        Console.WriteLine("Number should be 1-10");
+    }
+    else
+    {
+        Console.WriteLine("Wrong. Try Again");
+    }
 }
-else if (guessNumber >= 10)
-{
-    Console.WriteLine("Number should be 1-10");
-}
-else
-{
-    Console.WriteLine("Wrong. Try Again");
-}
+
